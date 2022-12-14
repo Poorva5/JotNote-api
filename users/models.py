7f3b6c66859c367b674ee61849c50f5095b7e29d
Poorva5 <poorvak55@gmail.com>
2022-12-14 13:25:58 +0530

    Updated auth feature

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    
+    def save(self, *args, **kwargs):
+        self.username = self.email
+        return super().save(*args, **kwargs)
+
---
+++ users/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from users.models import User
+
+class UserSerialzier(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'id', 'first_name', 'last_name']
